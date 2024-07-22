d204caa8e1cb7e035f8dac4fcd8ab1ed1e1e5437
kaustubdutt <kaustubdutt@gmail.com>
2024-07-22 02:43:56 -0400

    milestone_3

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,14 +1,19 @@
+import React, { useRef } from 'react';
 import { PROJECTS } from "../constants";
 import { motion } from "framer-motion";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
+import "swiper/css/navigation";
 import "swiper/css/free-mode";
-import { FreeMode, Pagination, Autoplay } from "swiper/modules";
-import { RxArrowTopRight } from "react-icons/rx";
+import { FreeMode, Pagination, Autoplay, Navigation } from "swiper/modules";
+import { HiOutlineArrowSmallLeft, HiOutlineArrowSmallRight } from "react-icons/hi2";
 import '../styles/globals.css'; // Import the custom CSS file
+import { DiGithubFull } from "react-icons/di";
 
 const Projects = () => {
+  const swiperRef = useRef(null);
+
   return (
     <div className="border-b border-neutral-600 pb-4" id="projects">
       <motion.h2
@@ -19,52 +24,74 @@ const Projects = () => {
       >
         Projects
       </motion.h2>
-      <Swiper
-        breakpoints={{
-          340: {
-            slidesPerView: 1,
-            spaceBetween: 15,
-          },
-          700: {
-            slidesPerView: 1,
-            spaceBetween: 15,
-          },
-        }}
-        freeMode={true}
-        autoplay={true}
-        pagination={{
-          clickable: true,
-        }}
-        modules={[FreeMode, Pagination, Autoplay]}
-        loop={true} // Enable infinite scrolling
-        className="max-w-[90%] lg:max-w-[80%]"
-      >
-        {PROJECTS.map((project, index) => (
-          <SwiperSlide key={index} className="p-4">
-            <div className="flex flex-col gap-4 mb-20 group relative text-white rounded-xl px-6 py-8 h-[500px] lg:h-[400px] overflow-hidden cursor-pointer shadow-custom hover:shadow-custom-hover">
-              <div className={project.backgroundClass}></div>
-              <div className="absolute inset-0 bg-black opacity-10 group-hover:opacity-30 rounded-xl" />
-              <div className="relative flex flex-col gap-3 z-10">
-                <h6 className="mb-2 font-semibold">{project.title}</h6>
-                <p className="mb-4 text-neutral-400">{project.description}</p>
-                <div className="flex flex-wrap gap-2">
-                  {project.technologies.map((tech, index) => (
-                    <span
-                      key={index}
-                      className="mr-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-300"
-                    >
-                      {tech}
-                    </span>
-                  ))}
+      <div className="relative max-w-[90%] lg:max-w-[80%] mx-auto">
+        <Swiper
+          ref={swiperRef}
+          breakpoints={{
+            340: {
+              slidesPerView: 1,
+              spaceBetween: 15,
+            },
+            700: {
+              slidesPerView: 1,
+              spaceBetween: 15,
+            },
+          }}
+          freeMode={true}
+          autoplay={{
+            delay: 3000, // Autoplay delay in milliseconds
+            disableOnInteraction: false, // Continue autoplay after interactions
+          }}
+          pagination={{
+            clickable: true,
+          }}
+          navigation={{
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+          }}
+          modules={[FreeMode, Pagination, Autoplay, Navigation]}
+          loop={true} // Enable infinite scrolling
+        >
+          {PROJECTS.map((project, index) => (
+            <SwiperSlide key={index} className="p-4 relative">
+              <div className="flex flex-col gap-4 mb-20 group relative text-white rounded-xl px-6 py-8 h-[500px] lg:h-[500px] overflow-hidden cursor-pointer shadow-custom hover:shadow-custom-hover">
+                <div className={project.backgroundClass}></div>
+                <div className="absolute inset-0 bg-black opacity-10 group-hover:opacity-30 rounded-xl" />
+                <div className="relative flex flex-col gap-3 z-10">
+                  <h6 className="mb-2 font-semibold">{project.title}</h6>
+                  <p className="mb-4 text-neutral-400">{project.description}</p>
+                  <div className="flex flex-wrap gap-2">
+                    {project.technologies.map((tech, index) => (
+                      <span
+                        key={index}
+                        className="mr-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-300"
+                      >
+                        {tech}
+                      </span>
+                    ))}
+                  </div>
                 </div>
+                <DiGithubFull className="absolute bottom-5 left-5 w-[35px] h-[35px] text-white group-hover:text-pink-500 duration-100" />
               </div>
-              <RxArrowTopRight className="absolute bottom-5 left-5 w-[35px] h-[35px] text-white group-hover:text-pink-500 group-hover:rotate-45 duration-100" />
-            </div>
-          </SwiperSlide>
-        ))}
-      </Swiper>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+        <button className = "absolute bottom-5 left-5 bg-transparent rounded-full p-2 shadow-md z-20 flex items-center justify-center"
+          onClick={() => swiperRef.current.swiper.slidePrev()}
+        >
+          <HiOutlineArrowSmallLeft className=" absolute w-[35px] h-[35px] text-white" />
+        </button>
+        <button className = "absolute bottom-5 right-5 bg-transparent rounded-full p-2 shadow-md z-20 flex items-center justify-center"
+          onClick={() => swiperRef.current.swiper.slideNext()}
+        >
+          <HiOutlineArrowSmallRight className=" absolute w-[35px] h-[35px] text-white" />
+        </button>
+      </div>
     </div>
   );
 };
 
 export default Projects;
+
+
+
